e5442fb9e13f8d1c0dc03c31446b218335b0c314
BAITA Enzo <eba2190@archimede.enseeiht.fr>
2024-12-05 15:57:11 +0100

    DiaryServer

---
+++ DiaryServer.java
@@ -0,0 +1,18 @@
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+
+public class DiaryServer {
+    public static void main(String[] args){
+        try {
+            //Initialisation de l'annuaire 
+            DiaryImpl diary = new DiaryImpl();
+            //Localisation ou création de l'annuaire sur le port 8080 et on enregistre l'objet diary sous le nom "Annuaire"
+            LocateRegistry.createRegistry(8080).rebind("Annuaire", diary);
+            //Debug 
+            System.out.println("Le serveur de l'annuaire tourne...");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
